ab2eba5f70e1537c78768fddba144d241048d58a
Andrey Rankov <rankov@gmail.com>
2021-07-30 21:42:07 +0300

    Display table rows w/ data only

--- lib/controller/details_controller.dart
+++ lib/controller/details_controller.dart
@@ -1,8 +1,100 @@
+import 'dart:ui';
+
+import 'package:carist/controller/data_controller.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class DetailsController extends GetxController {
+  var _base;
+  var _details;
+  var _extra;
+
+  var _detailsList = <RowDetails>[];
+
   @override
   void onInit() {
+    DataController _dataController = Get.find();
+    _base = _dataController.base.value;
+    _details = _dataController.details.value;
+    _extra = _dataController.extra.value;
+    _initDetailsList();
     super.onInit();
   }
+
+  void _initDetailsList() {
+    _addDetail(RowDetails('details_model'.tr, _vehicleName()));
+    _addDetail(RowDetails('details_version'.tr, _base.version));
+    _addDetail(RowDetails('details_year'.tr, _base.year));
+    _addDetail(RowDetails('details_engine_size'.tr, _details.engineSize,
+        units: 'details_cc'.tr));
+    _addDetail(RowDetails('details_fuel'.tr, _base.fuel));
+    _addDetail(RowDetails('details_horse_power'.tr, _details.horsePower));
+    _addDetail(RowDetails('details_ownership'.tr, _base.ownership));
+    _addDetail(RowDetails('details_weight'.tr, _details.weight,
+        units: 'details_kilo'.tr));
+    _addDetail(RowDetails('details_vin'.tr, _base.vin));
+    _addDetail(RowDetails('details_last_test'.tr, _formatDate(_base.testDate)));
+    _addDetail(RowDetails(
+        'details_license_valid'.tr, _formatDate(_base.licenseValidity),
+        textColor: _dateColor(_base.licenseValidity)));
+  }
+
+  void _addDetail(RowDetails detail) {
+    if (detail.value == null || detail.value == '' || detail.value == 0) return;
+    _detailsList.add(detail);
+  }
+
+  String _vehicleName() {
+    String brand = _extra.brandEng;
+    if (brand == null) return null;
+    String model = _base.model;
+    if (model.isCaseInsensitiveContains(brand)) {
+      return model;
+    }
+    return ('${brand.toUpperCase()} $model');
+  }
+
+  String _vehicleType(String type) {
+    String vehicleType = 'details_type_private'.tr;
+    if (type == 'M') {
+      vehicleType = 'details_type_commercial'.tr;
+    }
+    return vehicleType;
+  }
+
+  String _formatDate(String date) {
+    DateTime dateTime = DateTime.parse(date);
+    return DateFormat("dd/MM/yyyy").format(dateTime);
+  }
+
+  Color _dateColor(String date) {
+    return _daysLeft(date) < _daysInCurrentMonth(date)
+        ? Colors.red
+        : Colors.white;
+  }
+
+  int _daysLeft(String date) {
+    DateTime dateTime = DateTime.parse(date);
+    return dateTime.difference(DateTime.now()).inDays;
+  }
+
+  int _daysInCurrentMonth(String date) {
+    DateTime dateTime = DateTime.now();
+    return DateTime(dateTime.year, dateTime.month + 1, 0).day;
+  }
+
+  List<RowDetails> getDetails() {
+    return _detailsList;
+  }
+}
+
+class RowDetails {
+  String title;
+  var value;
+  Color textColor;
+  String units;
+
+  RowDetails(this.title, this.value,
+      {this.textColor = Colors.white, this.units = ''});
 }
--- lib/view/details_table.dart
+++ lib/view/details_table.dart
@@ -1,71 +1,48 @@
-import 'package:carist/controller/data_controller.dart';
-import 'package:carist/model/data.dart';
+import 'package:carist/controller/details_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 
 class DetailsTable extends StatelessWidget {
-  final DataController _dataController = Get.find();
+  final DetailsController detailsController = Get.put(DetailsController());
 
   @override
   Widget build(BuildContext context) {
-    Base base = _dataController.base.value;
-    Rx<Details> details = _dataController.details;
-
     return Expanded(
       child: SingleChildScrollView(
         scrollDirection: Axis.vertical,
-        child: Obx(
-          () => DataTable(
-            showCheckboxColumn: false,
-            headingRowHeight: 0,
-            dividerThickness: 2.0,
-            columns: [
-              DataColumn(label: Text("")),
-              DataColumn(label: Text("")),
-            ],
-            rows: [
-              _textRow('details_model'.tr, _vehicleName()),
-              _textRow('details_version'.tr, base.version),
-              _textRow('details_year'.tr, base.year),
-              _textRow(
-                'details_engine_size'.tr,
-                details.value.engineSize,
-                units: 'details_cc'.tr,
-              ),
-              _textRow('details_fuel'.tr, base.fuel),
-              _textRow('details_horse_power'.tr, details.value.horsePower),
-              _textRow('details_ownership'.tr, base.ownership),
-              _textRow(
-                'details_weight'.tr,
-                details.value.weight,
-                units: 'details_kilo'.tr,
-              ),
-              _textRow('details_vin'.tr, base.vin),
-              _textRow('details_last_test'.tr, _formatDate(base.testDate)),
-              _textRow(
-                'details_license_valid'.tr,
-                _formatDate(base.licenseValidity),
-                textColor: _dateColor(base.licenseValidity),
-              ),
-            ],
-          ),
+        child: DataTable(
+          showCheckboxColumn: false,
+          headingRowHeight: 0,
+          dividerThickness: 2.0,
+          columns: [
+            DataColumn(label: Text("")),
+            DataColumn(label: Text("")),
+          ],
+          rows: getRows(),
         ),
       ),
     );
   }
 
-  DataRow _textRow(String title, var value,
-      {Color textColor = Colors.white, String units = ''}) {
-    var data = value == null ? '-' : value.toString();
-    if (!units.isBlank) {
-      data = "$data $units";
+  List<DataRow> getRows() {
+    var rowsList = <DataRow>[];
+    var details = detailsController.getDetails();
+    details.forEach((element) {
+      rowsList.add(_dataRow(element));
+    });
+    return rowsList;
+  }
+
+  DataRow _dataRow(RowDetails detail) {
+    var data = detail.value.toString();
+    if (!detail.units.isBlank) {
+      data = "$data ${detail.units}";
     }
     return DataRow(
       cells: [
-        DataCell(Text(title)),
-        DataCell(Text(data, style: TextStyle(color: textColor))),
+        DataCell(Text(detail.title)),
+        DataCell(Text(data, style: TextStyle(color: detail.textColor))),
       ],
       onSelectChanged: (_) {
         Clipboard.setData(ClipboardData(text: data)).then((value) {
@@ -74,43 +51,4 @@ class DetailsTable extends StatelessWidget {
       },
     );
   }
-
-  String _vehicleName() {
-    String brand = _dataController.extra.value.brandEng;
-    if (brand == null) return null;
-    String model = _dataController.base.value.model;
-    if (model.isCaseInsensitiveContains(brand)) {
-      return model;
-    }
-    return ('${brand.toUpperCase()} $model');
-  }
-
-  String _formatDate(String date) {
-    DateTime dateTime = DateTime.parse(date);
-    return DateFormat("dd/MM/yyyy").format(dateTime);
-  }
-
-  Color _dateColor(String date) {
-    return _daysLeft(date) < _daysInCurrentMonth(date)
-        ? Colors.red
-        : Colors.white;
-  }
-
-  int _daysLeft(String date) {
-    DateTime dateTime = DateTime.parse(date);
-    return dateTime.difference(DateTime.now()).inDays;
-  }
-
-  int _daysInCurrentMonth(String date) {
-    DateTime dateTime = DateTime.now();
-    return DateTime(dateTime.year, dateTime.month + 1, 0).day;
-  }
-}
-
-String _vehicleType(String type) {
-  String vehicleType = 'details_type_private'.tr;
-  if (type == 'M') {
-    vehicleType = 'details_type_commercial'.tr;
-  }
-  return vehicleType;
 }
